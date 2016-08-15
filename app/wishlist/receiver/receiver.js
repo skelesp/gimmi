8068dfd7477fd4b3c92831cf3e830ba33d232970
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2016-08-15 23:59:59 +0200

    Changed layout

--- app/wishlist/receiver/receiver.js
+++ app/wishlist/receiver/receiver.js
@@ -4,12 +4,22 @@
 	'wishlist.receiver.create',
 	'wishlist.receiver.edit'
 ])
-	.controller('receiverCtrl', function(receiverModel) {
+	.controller('receiverCtrl', function($scope, $state, receiverModel) {
 		var receiverCtrl = this;
 
 		receiverModel.getReceivers()
 			.then(function(receivers) {
 				receiverCtrl.receivers = receivers;
 			});
+
+		receiverCtrl.showWishlist = function(keyEvent, name) {
+		  if (keyEvent.which === 13){
+				var text = name;
+
+				text = text.charAt(0).toUpperCase() + text.slice(1);
+				$state.go('gimmi.wishlist.wish',{receiverName: text});
+				$scope.selected = null;
+			}
+		};
 	})
 ;
