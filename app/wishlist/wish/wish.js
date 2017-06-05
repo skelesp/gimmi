23808e62bd6b232a6a1d7e507b7ce07be24f0fe3
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2017-06-05 17:44:22 +0200

    Reserveringsinfo toegevoegd in wishdetail

--- app/wishlist/wish/wish.js
+++ app/wishlist/wish/wish.js
@@ -20,14 +20,15 @@
 	})
 	.controller('wishInfoCtrl', ['$stateParams', 'wishModel', 'receiverModel', function ($stateParams, wishModel, receiverModel){
 			var _self = this;
-			
+
 			wishModel.getWishById($stateParams.wishID).then( function(wish){
 				_self.title = wish.title;
 				_self.price = wish.price;
 				_self.url = wish.url;
 				_self.image = wish.image;
-				_self.status = wish.status;
-				_self.reservedBy = wish.reservedBy;
+				_self.reservedBy = wish.reservation.reservedBy.fullName;
+				_self.createdBy = wish.createdBy.fullName;
+				_self.createdAt = wish.createdAt;
 				_self.receiver = receiverModel.getCurrentReceiverName();
 				_self.receiverID = wish.receiver;
 			});
