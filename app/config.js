0d245764d25bd4772c7140124f5f0a835ed4a0a6
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2017-09-10 17:44:36 +0200

    Changed local apiUrl for testing with mobile

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 angular.module('gimmi.config', [])
   .constant('CONFIG', {
     //apiUrl: 'https://gimmi.herokuapp.com'
-    apiUrl: 'http://localhost:5000',
+    apiUrl: 'http://192.168.0.136:5000',
     siteBaseUrl: 'http://localhost/projects/gimmi',
     defaultImage: 'https://image.flaticon.com/icons/png/128/214/214305.png'
   })
