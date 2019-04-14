ee6ef723039986bfbcf04bef7846d827fda8a53b
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2019-04-14 15:10:21 +0200

    add cards to userdashboard

--- app/people/userdashboard/userdashboard-ctrl.js
+++ app/people/userdashboard/userdashboard-ctrl.js
@@ -13,11 +13,41 @@ angular.module('userdashboard', [
         resolve: {
             user: ['PersonService', '$stateParams', function (PersonService, $stateParams){
                 return PersonService.getPersonFromID($stateParams.userID)
+            }],
+            wishlist: ['wishModel', '$stateParams', function(wishModel, $stateParams){
+                return wishModel.getWishlist($stateParams.userID);
             }]
         }
     });
 }])
-    .controller('userDashboardController', ['user', function (user) {
+    .controller('userDashboardController', ['$state', 'Flash', 'user', 'wishlist', function ($state, Flash, user, wishlist) {
     var self = this;
     self.user = user;
+    self.wishCount = function() {
+        var count = 0;
+        wishlist.wishes.forEach(wish => {
+            if (!wish.closure) {
+                count++;
+            }
+        });
+        return count;
+    }
+    self.toWishlist = function () {
+        $state.go('gimmi.wishlist', { receiverID: self.user._id });
+    };
+    self.toReservationManagement = function () {
+        showNotSupportedMessage();
+    };
+    self.startNewGiftSearch = function () {
+        showNotSupportedMessage();
+    };
+    self.addWish = function () {
+        showNotSupportedMessage();
+    };
+    self.toUserDashboard = function () {
+        $state.go('gimmi.userdashboard', { userID: self.user._id });
+    };
+    function showNotSupportedMessage(){
+        Flash.create('danger', 'Deze functie wordt nog niet ondersteund.');
+    };
 }]);
