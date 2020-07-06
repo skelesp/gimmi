bb3bca7302bfec2fd7e25d31531a436d6fa7ec16
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2020-07-06 22:35:01 +0200

    install vanilla angular 9 app

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gimmi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gimmi';
+}
