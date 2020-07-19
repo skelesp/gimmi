ad1af96b0fb1abb2615eb158f8224fa4da0c9579
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2020-07-19 04:21:14 +0200

    move person search out of "collapsable" navbar

--- src/app/shared/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'gimmi-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.css']
 })
-export class NavigationBarComponent implements OnInit {
-
-  constructor( ) { }
-
-  ngOnInit(): void {
-  }
+export class NavigationBarComponent {
+  public isMenuCollapsed = true;
 }
