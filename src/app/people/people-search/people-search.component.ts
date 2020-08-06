680ffeedd226782f19210ba32dd053a3b7a0df80
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2020-08-06 14:01:03 +0200

    search on click / focus of person search

--- src/app/people/people-search/people-search.component.ts
+++ src/app/people/people-search/people-search.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, NavigationEnd, RouterEvent } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject, merge } from 'rxjs';
 import { debounceTime, map, distinctUntilChanged, tap, filter } from "rxjs/operators";
 import { faSearch, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { IPerson } from '../models/person.model';
 import { PeopleService } from '../service/people.service';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'gimmi-people-search',
   templateUrl: './people-search.component.html',
   styleUrls: ['./people-search.component.css']
 })
-export class PeopleSearchComponent implements OnInit {
+export class PeopleSearchComponent implements OnInit {  
   userIcon = faUserCircle;
   searchIcon = faSearch;
   people: IPerson[] = [];
   selectedPerson: IPerson;
+
+  @ViewChild('instance', { static: true}) instance: NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
 
   constructor( private peopleService : PeopleService, private router : Router) { }
 
@@ -26,12 +31,17 @@ export class PeopleSearchComponent implements OnInit {
   }
 
   search = (text$: Observable<string>) : Observable<IPerson[]> => {
-    return text$.pipe(
+    const debouncedText$ = text$.pipe(
       debounceTime(200),
-      distinctUntilChanged(),
+      distinctUntilChanged()
+    );
+    const clickWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+    const inputFocused$ = this.focus$;
+    
+    return merge(debouncedText$, clickWithClosedPopup$, this.focus$).pipe(
       map(term => term === '' ? [] : this.people
-                                            .filter(person => person.fullName.toLowerCase().indexOf(term.toLowerCase()) > -1)
-                                            .slice(0, 10)
+        .filter(person => person.fullName.toLowerCase().indexOf(term.toLowerCase()) > -1)
+        .slice(0, 10)
       )
     );
   }
