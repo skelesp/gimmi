30d4783301c2b7ba09ecc546ebcac37784da3724
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2020-10-26 13:04:10 +0100

    create basic registration form

--- src/app/users/components/register/register.component.ts
+++ src/app/users/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'gimmi-register',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  invitedFor: string;
+  knownUser: User;
+  registrationForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.invitedFor = null; // Wordt pas geïmplementeerd als wishlist geïmplementeerd worden.
+    this.registrationForm = new FormGroup ({
+      'personData': new FormGroup({
+        'firstName': new FormControl(null, Validators.required),
+        'lastName': new FormControl(null, Validators.required),
+        'birthday': new FormControl(null, Validators.required)
+      }),
+      'localAccountData': new FormGroup({
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'password': new FormControl(null, [Validators.required])
+      })
+    });
+  }
+
+  register () {
+    console.log(this.registrationForm.value);
   }
 
 }
