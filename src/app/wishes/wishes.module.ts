e042585dc1fb0573456353cf7c1fed4edb606346
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2020-11-07 01:14:09 +0100

    Add shared module to wishes module

--- src/app/wishes/wishes.module.ts
+++ src/app/wishes/wishes.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { WishesRoutingModule } from './wishes-routing.module';
 import { WishListComponent } from './components/wish-list/wish-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [WishListComponent],
   imports: [
     CommonModule,
+    SharedModule,
     WishesRoutingModule
   ],
   exports: [
