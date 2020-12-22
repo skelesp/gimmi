a3d569de25629b7cd16cd163a9e236ce283f9be9
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2020-12-23 00:36:21 +0100

    generic wish scenario concept added

--- src/app/wishes/models/wish.model.ts
+++ src/app/wishes/models/wish.model.ts
@@ -1,7 +1,9 @@
 import { Person } from 'src/app/people/models/person.model';
+import { User } from 'src/app/users/models/user.model';
 import { environment } from 'src/environments/environment';
 
 export type wishStatus = 'Open' | 'Reserved' | 'Received' | 'Fulfilled';
+export type WishScenario = 'OPEN_WISH' | 'OPEN_WISH_CREATED_BY_USER_FOR_ANOTHER' | 'RESERVED' | 'RESERVED_BY_USER' | 'RESERVED_INCOGNITO_FOR_USER' | 'RECEIVED' | 'RECEIVED_GIVEN_BY_USER' | 'FULFILLED' | 'FULFILLED_BY_USER';
 
 export interface ICloudinaryImage {
     publicId: string,
@@ -9,7 +11,7 @@ export interface ICloudinaryImage {
 }
 
 export interface IReservation {
-    reservedBy: Person;
+    reservedBy: string;
     amount: number;
     reservationDate: Date;
     reason: string;
@@ -34,6 +36,11 @@ export class Wish {
     public giftFeedback: IGiftFeedback;
     public closure: IClosure;
     public copyOf: string;
+    private _userIs : {[key: string]:boolean} = {
+        receiver: null,
+        creator: null,
+        reservator: null
+    };
 
     constructor(
         public id: string,
@@ -53,10 +60,60 @@ export class Wish {
     
     public set status (v : wishStatus) {
         this._status = v;
-    }
-    
+    }    
     public get status() : wishStatus {
         return this._status;
+    }
+    public get userIsReceiver(): boolean {
+        return this._userIs.receiver;
+    }
+    public get userIsCreator(): boolean {
+        return this._userIs.creator;
+    } 
+    public get userIsReservator(): boolean {
+        return this._userIs.reservator;
+    }
+
+    public setUserIsFlags(user: User) {
+        this._userIs = {
+            receiver: user.id === this.receiver.id,
+            creator: user.id === this.createdBy.id,
+            reservator: user.id === this.reservation?.reservedBy
+        }
+    }
+
+    public get scenario() : WishScenario {
+        switch (this._status) {
+            case 'Open':
+                if (!this._userIs.receiver && this._userIs.creator) {
+                   return 'OPEN_WISH_CREATED_BY_USER_FOR_ANOTHER';
+                } else {
+                    return 'OPEN_WISH';
+                }
+            case 'Reserved':
+                if (this._userIs.reservator) {
+                    return 'RESERVED_BY_USER'
+                } else if (!this._userIs.receiver) {
+                    return 'RESERVED'
+                } else { // user is the receiver and should not know that something is reserved on his list
+                    return 'RESERVED_INCOGNITO_FOR_USER'
+                }
+            case 'Received':
+                if (this._userIs.reservator) {
+                    return 'RECEIVED_GIVEN_BY_USER'
+                } else {
+                    return 'RECEIVED'
+                }
+            case 'Fulfilled':
+                if (this._userIs.reservator) {
+                    return 'FULFILLED_BY_USER'
+                } else {
+                    return 'FULFILLED'
+                }
+            default:
+                console.warn(`No scenario set for wish with status ${this._status} and flags (receiver|creator|reservator): ${this._userIs.receiver}|${this._userIs.creator}|${this._userIs.reservator} `)
+                break;
+        }
     }
     
     
