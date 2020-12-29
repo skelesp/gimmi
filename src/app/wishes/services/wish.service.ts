eacbe395c7bec56831374f6591d8a0f5b8d594b0
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2020-12-29 01:41:05 +0100

    refactor getWishlist pipe for readability and maintenance

--- src/app/wishes/services/wish.service.ts
+++ src/app/wishes/services/wish.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, forkJoin, of } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, defaultIfEmpty, map, switchMap } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
 import { Person } from 'src/app/people/models/person.model';
@@ -60,59 +60,63 @@ export class WishService {
     return this.http$.get<IWishlistResponse[]>(
       environment.apiUrl + 'wishlist/' + receiver.id
     ).pipe(
-      // Create wish instances from each wish in API response and save reservation for later use
-      map( wishlistResponse => 
-        wishlistResponse[0].wishes.map(wish => ({
-          wish: this.createWishInstanceFromResponse(wish, receiver),
-          reservation: wish.reservation
-        }))),
-      // For each wish with reservation: get person info for 'reservedBy' id
-      map( wishesAndReservationObjects => wishesAndReservationObjects.map( ({wish, reservation}) => 
-        !reservation ? 
-        of(wish) : 
-        this.peopleService.getPersonById(reservation.reservedBy)
-        .pipe(
-          map ( reservedBy => {
-            if (reservedBy) wish.reservation = { 
-              ...reservation,   
-              reservedBy: new Person(reservedBy.id, reservedBy.firstName, reservedBy.lastName)
-            }
-            return wish;
-          }),
-          catchError( (error : HttpErrorResponse) => {
-            console.error(`[wishService] ${error}`);
-            wish.reservation = {...reservation, reservedBy: new Person(reservation.reservedBy, 'not', 'found')}
-            return of(wish);
-          })
+      // Create wish instances from each wish in API response and save reservation for later use and retrieve reservedBy Person object
+      switchMap( wishlistResponse => forkJoin(
+        wishlistResponse[0].wishes.map(wishResponse =>
+          this.addReservedByToWishReservation(
+            this.createWishInstanceFromResponse(wishResponse, receiver),
+            wishResponse.reservation
+          )
         )
-       )),
-      // forkJoin all observables, so the result is an array of all the wishes
-      switchMap(reservedByObservables => reservedByObservables.length !== 0 ? forkJoin(reservedByObservables) : of(<Wish[]>[])), //https://stackoverflow.com/questions/41723541/rxjs-switchmap-not-emitting-value-if-the-input-observable-is-empty-array
+      )),
       // Call method on each wish (with or without reservation) to set user flags in each instance (must be done after reservedBy is added)
       map ( wishes => wishes.map( wish => {
         wish.setUserIsFlags(this.userService.currentUser);
         return wish;
       })),
       // For each wish: get state via API call
-      map ( wishesWithoutState => wishesWithoutState.map( wishWithoutState => 
-        this.http$.get<wishStatus>(environment.apiUrl + 'wish/' + wishWithoutState.id + '/state')
-        .pipe(
-          map( state => {
-            wishWithoutState.status = state;
-            return wishWithoutState;
-          }),
-          catchError((error: HttpErrorResponse) => {
-            console.error(`[wishService] ${error}`);
-            wishWithoutState.status = 'Open';
-            return of(wishWithoutState);
-          })
-        )
+      switchMap( wishesWithoutState => forkJoin(
+        wishesWithoutState.map( wishWithoutState => this.addStateToWish(wishWithoutState) )
       )),
-      // Combine all stateObservables into 1 array
-      switchMap(stateObservables => stateObservables.length !== 0 ? forkJoin(stateObservables) : of(<Wish[]>[]))
+      // Default case if observable returns null
+      defaultIfEmpty(<Wish[]>[])
     )
   }
 
+  private addReservedByToWishReservation ( wish: Wish, reservation ) :  Observable<Wish>{
+    return !reservation ? of(wish) :
+      this.peopleService.getPersonById(reservation.reservedBy)
+        .pipe(
+          map(reservedBy => {
+            if (reservedBy) wish.reservation = {
+              ...reservation,
+              reservedBy: new Person(reservedBy.id, reservedBy.firstName, reservedBy.lastName)
+            }
+            return wish;
+          }),
+          catchError((error: HttpErrorResponse) => {
+            console.error(`[wishService] ${error}`);
+            wish.reservation = { ...reservation, reservedBy: new Person(reservation.reservedBy, 'not', 'found') }
+            return of(wish);
+          })
+        )
+  }
+
+  private addStateToWish (wishWithoutState : Wish) : Observable<Wish> {
+    return this.http$.get<wishStatus>(environment.apiUrl + 'wish/' + wishWithoutState.id + '/state')
+      .pipe(
+        map(state => {
+          wishWithoutState.status = state;
+          return wishWithoutState;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          console.error(`[wishService] ${error}`);
+          wishWithoutState.status = 'Open';
+          return of(wishWithoutState);
+        })
+      )
+  }
+  
   private createWishInstanceFromResponse ( wishResponse : IWishResponse, receiver: Person ) : Wish {
     let wish : Wish = new Wish (
       wishResponse._id,
