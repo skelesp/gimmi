d3f3e86fea36080f60b7ea955b0af15256028621
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2020-12-30 13:44:14 +0100

    add dummy onClick actions depending on scenario

--- src/app/wishes/components/wish-item/wish-call-to-action-button/wish-call-to-action-button.component.ts
+++ src/app/wishes/components/wish-item/wish-call-to-action-button/wish-call-to-action-button.component.ts
@@ -6,27 +6,9 @@ import { WishScenario } from 'src/app/wishes/models/wish.model';
 
 interface CTAButtonConfig {
   text: string;
-  icon: IconDefinition
+  icon: IconDefinition;
+  onClick?: () => void;
 }
-
-const noButtonConfig: CTAButtonConfig = { text: null, icon: null };
-const reserveButtonConfig: CTAButtonConfig = { text: "Reserveer", icon: faGift };
-const cancelButtonConfig: CTAButtonConfig = { text: "Wijzig reservatie", icon: faBan };
-const feedbackButtonConfig: CTAButtonConfig = { text: "Geef feedback", icon: faComment };
-
-const buttonConfigs: { [key in WishScenario]: CTAButtonConfig } = {
-  'OPEN_WISH': reserveButtonConfig,
-  'OPEN_WISH_CREATED_BY_USER_FOR_ANOTHER': reserveButtonConfig,
-  'RESERVED': noButtonConfig,
-  'RESERVED_BY_USER': cancelButtonConfig,
-  'RESERVED_INCOGNITO_FOR_USER': reserveButtonConfig,
-  'RECEIVED': noButtonConfig,
-  'RECEIVED_RECEIVER': feedbackButtonConfig,
-  'RECEIVED_GIVEN_BY_USER': noButtonConfig,
-  'FULFILLED': noButtonConfig,
-  'FULFILLED_BY_USER': noButtonConfig
-};
-
 @Component({
   selector: 'gimmi-wish-call-to-action-button',
   templateUrl: './wish-call-to-action-button.component.html',
@@ -36,14 +18,42 @@ export class WishCallToActionButtonComponent implements OnInit, OnChanges {
   @Input() scenario : WishScenario;
   config: CTAButtonConfig = { text: null, icon: null };
   
-  constructor() { }
+  readonly noButtonConfig: CTAButtonConfig = { text: null, icon: null };
+  readonly reserveButtonConfig: CTAButtonConfig = { text: "Reserveer", icon: faGift, onClick: this.reserve };
+  readonly cancelButtonConfig: CTAButtonConfig = { text: "Wijzig reservatie", icon: faBan, onClick: this.changeReservation };
+  readonly feedbackButtonConfig: CTAButtonConfig = { text: "Geef feedback", icon: faComment, onClick: this.giveFeedback };
+
+  readonly buttonConfigs: { [key in WishScenario]: CTAButtonConfig } = {
+    'OPEN_WISH': this.reserveButtonConfig,
+    'OPEN_WISH_CREATED_BY_USER_FOR_ANOTHER': this.reserveButtonConfig,
+    'RESERVED': this.noButtonConfig,
+    'RESERVED_BY_USER': this.cancelButtonConfig,
+    'RESERVED_INCOGNITO_FOR_USER': this.reserveButtonConfig,
+    'RECEIVED': this.noButtonConfig,
+    'RECEIVED_RECEIVER': this.feedbackButtonConfig,
+    'RECEIVED_GIVEN_BY_USER': this.noButtonConfig,
+    'FULFILLED': this.noButtonConfig,
+    'FULFILLED_BY_USER': this.noButtonConfig
+  };
+
+  constructor() {  }
 
   ngOnInit(): void {
-    this.config = buttonConfigs[this.scenario];
+    this.config = this.buttonConfigs[this.scenario];
   }
 
   ngOnChanges(): void {
-    this.config = buttonConfigs[this.scenario];    
+    this.config = this.buttonConfigs[this.scenario];    
+  }
+
+  reserve () {
+    alert("Reserveer");
+  }
+  changeReservation() {
+    alert("Wijzig reservatie");
+  }
+  giveFeedback(){
+    alert("Geef feedback");
   }
 
 }
