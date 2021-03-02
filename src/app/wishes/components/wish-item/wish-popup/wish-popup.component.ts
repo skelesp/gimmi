50535892f6237f1e27d7cd3ce88433f6291dc68c
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2021-03-02 23:51:53 +0100

    add wish form (w/ dummy image button)

--- src/app/wishes/components/wish-item/wish-popup/wish-popup.component.ts
+++ src/app/wishes/components/wish-item/wish-popup/wish-popup.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Wish } from 'src/app/wishes/models/wish.model';
 
 @Component({
   selector: 'gimmi-wish-popup',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wish-popup.component.css']
 })
 export class WishPopupComponent implements OnInit {
+  @Input() wish : Wish;
+  wishForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public activeModal : NgbActiveModal
+  ){}
 
   ngOnInit(): void {
+    this.wishForm = new FormGroup({
+      'title': new FormControl(null, [Validators.required]),
+      'price': new FormControl(null),
+      'image': new FormControl(null),
+      'url': new FormControl(null),
+      'color': new FormControl(null),
+      'size': new FormControl(null),
+      'description': new FormControl(null),
+    });
+  }
+  updateWish () {
+    console.log(this.wishForm.value);
   }
 
 }
