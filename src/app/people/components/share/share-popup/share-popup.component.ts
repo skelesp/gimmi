4e26ec2895d934f463f63220b60e3ea8d8db5a95
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2021-11-13 00:27:30 +0100

    add check if user is logged in with Facebook

--- src/app/people/components/share/share-popup/share-popup.component.ts
+++ src/app/people/components/share/share-popup/share-popup.component.ts
@@ -1,24 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { Person } from 'src/app/people/models/person.model';
+import { User } from 'src/app/users/models/user.model';
+import { UserService } from 'src/app/users/service/user.service';
 
 @Component({
   selector: 'gimmi-share-popup',
   templateUrl: './share-popup.component.html',
   styleUrls: ['./share-popup.component.css']
 })
-export class SharePopupComponent {
+export class SharePopupComponent implements OnInit, OnDestroy {
   @Input() linkToShare : string;
   @Input() personToShare : Person;
-  
+  currentUser: User; currentUserSubscription: Subscription;
   message: string = 'Hey, op volgende link vind je mijn wensenlijst voor een cadeautje.';
   
   constructor(
-    public activeModal: NgbActiveModal
+    public activeModal: NgbActiveModal,
+    private userService: UserService
   ) { }
 
+  ngOnInit(): void {
+    this.currentUserSubscription = this.userService.currentUser$.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
+  
   generateMessage () : string {
     return encodeURIComponent(`${this.message} ${this.linkToShare}`);
   }
-
+  
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe();
+  }
 }
