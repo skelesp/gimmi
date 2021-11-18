88c0810497690dbb9473a34052b83748099d8705
Stijn Beeckmans <stijn.beeckmans@gmail.com>
2021-11-18 23:16:13 +0100

    add reporting module

---
+++ src/app/reporting/components/lean-startup/lean-startup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gimmi-lean-startup',
+  templateUrl: './lean-startup.component.html',
+  styleUrls: ['./lean-startup.component.css']
+})
+export class LeanStartupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
